c04ef48684b893a9d6282a9484b35b4617ee7e81
KaoImin <kaoimin@qq.com>
2019-04-29 16:19:07 +0800

    add docs

--- src/types.rs
+++ src/types.rs
@@ -4,47 +4,48 @@ use rlp::{Decodable, Encodable, RlpStream};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::collections::HashMap;
 
-///
+/// Address type.
 pub type Address = Vec<u8>;
-///
+/// Hash type.
 pub type Hash = Vec<u8>;
 
-///
+/// Consensus input message types.
 #[derive(Debug, Clone)]
 pub enum ConsensusInput<F: Content + Sync> {
-    ///
+    /// Signed proposal message.
     SignedProposal(SignedProposal<F>),
-    ///
+    /// Signed vote message.
     SignedVote(SignedVote),
-    ///
+    /// Rich status message.
     Status(Status),
 }
 
+/// Async message type.
 #[derive(Debug, Clone)]
 pub(crate) enum AsyncMsg<F: Content + Sync> {
-    ///
+    /// Verify response of a proposal.
     VerifyResp(VerifyResp),
-    ///
+    /// Transcation set of a height.
     Feed(Feed<F>),
 }
 
-///
+/// Consensus output message.
 #[derive(Debug, Clone)]
 pub enum ConsensusOutput<F: Content + Sync> {
-    ///
+    /// Signed proposal message.
     SignedProposal(SignedProposal<F>),
-    ///
+    /// Signed vote message.
     SignedVote(SignedVote),
-    ///
+    /// Commit message.
     Commit(Commit<F>),
 }
 
-///
+/// Vote types.
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, Hash)]
 pub enum VoteType {
-    ///
+    /// Prevote vote type.
     Prevote,
-    ///
+    /// Precommit vote type.
     Precommit,
 }
 
@@ -57,34 +58,34 @@ impl Into<u8> for VoteType {
     }
 }
 
-///
+/// Signed proposal type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct SignedProposal<F: Content + Sync> {
-    ///
+    /// A proposal.
     #[serde(bound(deserialize = "F: DeserializeOwned"))]
     pub proposal: Proposal<F>,
-    ///
+    /// A signature of the proposal.
     pub signature: Vec<u8>,
 }
 
-///
+/// Proposal type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Proposal<F: Content + Sync> {
-    ///
+    /// The height of a proposal.
     pub height: u64,
-    ///
+    /// The round of a proposal.
     pub round: u64,
-    ///
+    /// The content of a proposal.
     #[serde(bound(deserialize = "F: DeserializeOwned"))]
     pub content: F,
-    ///
+    /// The proof of a proposal.
     #[serde(bound(deserialize = "F: DeserializeOwned"))]
     pub proof: Proof<F>,
-    ///
+    /// The lock round of a proposal. If the proposal has not been locked, it should be `None`.
     pub lock_round: Option<u64>,
-    ///
+    /// The lock votes of a proposal. If the proposal has not been locked, it should be an empty `Vec`.
     pub lock_votes: Vec<SignedVote>,
-    ///
+    /// The address of proposer.
     pub proposer: Address,
 }
 
@@ -107,8 +108,8 @@ impl<F> Proposal<F>
 where
     F: Content + Sync,
 {
-    ///
-    pub fn to_bft_proposal(&self, hash: Vec<u8>) -> bft::Proposal {
+    /// A function to convert a proposal into the corresponding type in BFT-core.
+    pub(crate) fn to_bft_proposal(&self, hash: Vec<u8>) -> bft::Proposal {
         let lock_votes = if self.lock_round.is_some() {
             let mut res = Vec::new();
             for vote in self.lock_votes.iter() {
@@ -130,12 +131,12 @@ where
     }
 }
 
-///
+/// Signed vote type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct SignedVote {
-    ///
+    /// A vote.
     pub vote: Vote,
-    ///
+    /// A signature of the vote.
     pub signature: Vec<u8>,
 }
 
@@ -145,18 +146,18 @@ impl Encodable for SignedVote {
     }
 }
 
-///
+/// Vote type.
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub struct Vote {
-    ///
+    /// The vote type of a vote.
     pub vote_type: VoteType,
-    ///
+    /// The height of a vote.
     pub height: u64,
-    ///
+    /// The round of a vote.
     pub round: u64,
-    ///
+    /// The proposal of a vote.
     pub proposal: Hash,
-    ///
+    /// The address of voter.
     pub voter: Address,
 }
 
@@ -172,6 +173,7 @@ impl Encodable for Vote {
 }
 
 impl Vote {
+    /// A function to convert vote into the corresponding type in BFT-core.
     pub(crate) fn to_bft_vote(&self) -> bft::Vote {
         let vote_type = if self.vote_type == VoteType::Prevote {
             bft::VoteType::Prevote
@@ -188,6 +190,7 @@ impl Vote {
         }
     }
 
+    /// A function to convert vote from the corresponding type in BFT-core.
     pub(crate) fn from_bft_vote(vote: bft::Vote, vtype: VoteType) -> Vote {
         Vote {
             vote_type: vtype,
@@ -199,40 +202,40 @@ impl Vote {
     }
 }
 
-///
+/// Commit type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Commit<F: Encodable + Decodable + Clone + Send + 'static + Serialize + DeserializeOwned>
 {
-    ///
+    /// The height of a commit.
     pub height: u64,
-    ///
+    /// The consensus result.
     #[serde(bound(deserialize = "F: DeserializeOwned"))]
     pub result: F,
-    ///
+    /// The previous hash.
     pub prev_hash: Hash,
-    ///
+    /// The proof of the commit.
     #[serde(bound(deserialize = "F: DeserializeOwned"))]
     pub proof: Proof<F>,
-    ///
+    /// The address of the node.
     pub address: Address,
 }
 
-///
+/// Rich status type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Status {
-    ///
+    /// The height of a status.
     pub height: u64,
-    ///
+    /// The block hash of the height.
     pub prev_hash: Hash,
-    ///
+    /// The consensus interval. If it is `None`, use the default interval that is 3 seconds.
     pub interval: Option<u64>,
-    ///
+    /// The authority of the next height.
     pub authority_list: Vec<Node>,
 }
 
 impl Status {
-    ///
-    pub fn to_bft_status(&self) -> bft::Status {
+    /// A function to convert a rich status into the corresponding type in BFT-core.
+    pub(crate) fn to_bft_status(&self) -> bft::Status {
         let mut res = Vec::new();
         for node in self.authority_list.iter() {
             res.push(node.address.to_owned());
@@ -245,7 +248,7 @@ impl Status {
     }
 }
 
-///
+/// Feed type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Feed<F: Encodable + Decodable + Clone + Send + 'static + Serialize + DeserializeOwned> {
     /// The height of the proposal.
@@ -255,16 +258,17 @@ pub struct Feed<F: Encodable + Decodable + Clone + Send + 'static + Serialize + 
     pub content: F,
 }
 
-///
+/// Verify response type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
-pub struct VerifyResp {
-    ///
-    pub is_pass: bool,
-    ///
-    pub proposal: Hash,
+pub(crate) struct VerifyResp {
+    /// A result of verify.
+    pub(crate) is_pass: bool,
+    /// A proposal.
+    pub(crate) proposal: Hash,
 }
 
 impl VerifyResp {
+    /// A function to convert verify response into the corresponding type in BFT-core.
     pub(crate) fn to_bft_resp(&self) -> bft::VerifyResp {
         bft::VerifyResp {
             is_pass: self.is_pass,
@@ -273,14 +277,14 @@ impl VerifyResp {
     }
 }
 
-///
+/// The authority manage type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct AuthorityManage {
-    ///
+    /// The authority list at present.
     pub authorities: Vec<Node>,
-    ///
+    /// An old authority.
     pub authorities_old: Vec<Node>,
-    ///
+    /// The height of the old authority.
     pub authority_h_old: u64,
 }
 
@@ -295,6 +299,7 @@ impl Default for AuthorityManage {
 }
 
 impl AuthorityManage {
+    /// A function to update authority list.
     pub(crate) fn update_authority(&mut self, h: u64, auth_list: Vec<Node>) {
         let tmp = self.authorities.clone();
         self.authorities = auth_list;
@@ -303,19 +308,19 @@ impl AuthorityManage {
     }
 }
 
-///
+/// Node type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Node {
-    ///
+    /// The address of a node.
     pub address: Address,
-    ///
+    /// The proposal weight of a node.
     pub proposal_weight: u32,
-    ///
+    /// The vote weight of a node.
     pub vote_weight: u32,
 }
 
 impl Node {
-    ///
+    /// A function to generate a `Node` with default proposal weight and vote weight.
     pub fn new(address: Address) -> Self {
         Node {
             address,
@@ -324,23 +329,23 @@ impl Node {
         }
     }
 
-    ///
+    /// A function to get the address of a node.
     pub fn get_address(&self) -> Address {
         self.address.clone()
     }
 }
 
-///
+/// Proof type.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
 pub struct Proof<F: Encodable + Decodable + Clone + Send + 'static + Serialize + DeserializeOwned> {
-    ///
+    /// The hash of a proof.
     #[serde(bound(deserialize = "F: DeserializeOwned"))]
     pub block_hash: F,
-    ///
+    /// The height of votes in the proof.
     pub height: u64,
-    ///
+    /// The rounf of votes in the proof.
     pub round: u64,
-    ///
+    /// The precommit vote set of the proof.
     pub precommit_votes: HashMap<Address, Vec<u8>>,
 }
 
@@ -362,15 +367,4 @@ where
             s.append(&v.0).append(&v.1);
         }
     }
-}
-
-///
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
-pub struct LockStatus {
-    ///
-    pub block: Hash,
-    ///
-    pub round: u64,
-    ///
-    pub votes: Vec<SignedVote>,
 }
