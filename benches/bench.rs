3166807cc12f3ffc6f0f7425532188fb86132179
KaoImin <kaoimin@qq.com>
2019-04-29 15:43:52 +0800

    refactor: variable name

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,17 +6,18 @@ use blake2b_simd::Params;
 use criterion::{Benchmark, Criterion};
 use crossbeam_channel::unbounded;
 use rand::random;
-// use sha3::{Digest, Sha3_256};
+use std::collections::HashMap;
 
 #[inline(always)]
 fn gen_mb(size: usize) -> Vec<u8> {
-    let mb: Vec<u8> = (0..1024 * 1024 * size).map(|_| random::<u8>()).collect();
-    mb
+    let data: Vec<u8> = (0..1024 * 1024 * size).map(|_| random::<u8>()).collect();
+    data
 }
 
 fn convert_proposal(content: Vec<u8>) {
     let (s, r) = unbounded();
     let mut short = Vec::new();
+    let mut cache = HashMap::new();
     if content.len() > 100 {
         let a = (content.len() as f64 / 100 as f64).round() as usize;
         for i in 0..100 {
@@ -33,6 +34,8 @@ fn convert_proposal(content: Vec<u8>) {
         .to_owned();
     s.send(hash.clone()).unwrap();
     r.recv().unwrap();
+    cache.entry(hash.clone()).or_insert(content);
+    let _ = cache.get(&hash);
     s.send(hash).unwrap();
     r.recv().unwrap();
 }
@@ -67,50 +70,3 @@ fn benchmark_2(c: &mut Criterion) {
 
 criterion_group!(benches, benchmark_1, benchmark_2);
 criterion_main!(benches);
-
-// fn bench_to_proposal(content: Vec<u8>) {
-//     let (s, r) = unbounded();
-//     let proof = Proof {
-//         block_hash: bench_proof(),
-//         height: 0,
-//         round: 0,
-//         precommit_votes: HashMap::new(),
-//     };
-
-//     let proposal = Proposal {
-//         height: 0,
-//         round: 0,
-//         content,
-//         proof,
-//         lock_round: None,
-//         lock_votes: Vec::new(),
-//         proposer: vec![3],
-//     };
-//     let res = proposal.to_bft_proposal(vec![4]);
-//     s.send(res).unwrap();
-//     r.recv().unwrap();
-// }
-
-// fn bench_proposal(content: Vec<u8>) {
-//     let (s, r) = unbounded();
-//     let res = SignedProposal {
-//         signature: vec![1, 1, 1],
-//         proposal: Proposal {
-//             height: 0,
-//             round: 0,
-//             content,
-//             proof: Proof {
-//                 block_hash: bench_proof(),
-//                 height: 0,
-//                 round: 0,
-//                 precommit_votes: HashMap::new(),
-//             },
-//             lock_round: None,
-//             lock_votes: Vec::new(),
-//             proposer: vec![3],
-//         },
-//     };
-
-//     s.send(res).unwrap();
-//     r.recv().unwrap();
-// }
